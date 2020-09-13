e0f2250e2e1419989986be069485f38b4509cf5e
longfangsong <longfangsong@icloud.com>
2020-09-13 19:29:44 +0800

    For debug

--- src/handler.rs
+++ src/handler.rs
@@ -2,15 +2,21 @@ use actix_web::{HttpRequest, Responder, HttpResponse};
 use crate::token::get_username;
 use crate::CONFIG;
 use actix_http::http::Method;
+use actix_web::http::Uri;
 
 pub async fn ping(_: HttpRequest) -> impl Responder {
     "pong"
 }
 
 pub async fn check(request: HttpRequest) -> impl Responder {
-    for header in request.headers() {
-        println!("{:?}", header);
-    }
+    println!("{:?}", request.method());
+    let forwarded_uri = request.headers().get("x-forwarded-uri")
+        .and_then(|it| it.to_str().ok())
+        .and_then(|it| it.parse::<Uri>().ok());
+    let (path, query) = match &forwarded_uri {
+        Some(uri) => (uri.path(), uri.query().unwrap_or("")),
+        None => (request.path(), request.query_string())
+    };
     let username = request.headers().get("Authorization")
         .and_then(|it| it.to_str().ok())
         .map(|it| &it["Bearer ".len()..])
@@ -18,9 +24,9 @@ pub async fn check(request: HttpRequest) -> impl Responder {
     let write = match request.method() {
         &Method::POST | &Method::PUT | &Method::DELETE => true,
         &Method::GET => false,
-        _ => unimplemented!()
+        _ => return HttpResponse::UnprocessableEntity()
     };
-    let passed = CONFIG.check(username.as_ref(), request.path(), request.query_string(), write);
+    let passed = CONFIG.check(username.as_ref(), path, query, write);
     if passed {
         info!("Allow {} to visit {}", username.unwrap_or("anonymous user".to_string()), request.path());
         HttpResponse::Ok()
